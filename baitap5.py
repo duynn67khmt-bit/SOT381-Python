03e8118f8c1ffcc7b64aed53b3edcc066eeb0f51
duynn67khmt-bit <duy.nn.67khmt@ntu.edu.vn>
2025-11-19 18:10:35 +0700

    Revert "diện tích hình thang"
    This reverts commit 7b649ab50a818fb913bcbbd6d29fb630c0b8f142.

--- baitap5.py
+++
@@ -1,6 +0,0 @@
-import math 
-a = float(int(input("nhập độ dài cạnh đáy a: ")))
-b = float(int(input("nhập độ dài cạnh đáy b: ")))
-h = float(int(input("nhập độ dài chiều cao h: ")))
-S = ((a + b) * h) / 2
-print("Diện tích hình thang là:", S)
--- baitap6.py
+++
@@ -1,7 +0,0 @@
-import math
-a = float(int(input("nhập độ dài cạnh a: ")))   
-b = float(int(input("nhập độ dài cạnh b: ")))
-c = float(int(input("nhập độ dài cạnh c: ")))   
-p = (a + b + c) / 2
-S = math.sqrt(p * (p - a) * (p - b) * (p - c))
-print("Diện tích tam giác là:", S)
