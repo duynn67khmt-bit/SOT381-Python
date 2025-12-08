c28296c0acfe9e0aa96bbb2d5f612a6de0c49d57
duynn67khmt-bit <duy.nn.67khmt@ntu.edu.vn>
2025-12-08 14:38:15 +0700

    Create baitap11.py

---
+++ baitap11.py
@@ -0,0 +1,6 @@
+import math
+a1 = float(int(input("Nhập số hạng đầu tiên a1: ")))
+d = float(int(input("Nhập công sai d: ")))
+n = int(input("Nhập số hạng n: "))
+S = n * (2*a1 + (n-1)*d) / 2
+print("tong cap so cong la:", S)
