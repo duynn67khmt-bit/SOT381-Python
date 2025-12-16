af4b49450d30f01f39a558945af5676ac1a313b9
Nduy <duy.nn.67khmt@ntu.edu.vn>
2025-12-16 23:29:01 +0700

    Create tinhsogiolamviecvatienluong.py

---
+++ tinhsogiolamviecvatienluong.py
@@ -0,0 +1,7 @@
+h = int(input("nhập vào số giờ làm việc mỗi ngày: "))
+d = int(input("nhập vào số ngày làm việc trong tháng: "))
+
+print("Tổng số giờ làm việc trong tháng là:", h * d)
+
+s = int(input("nhập vào số tiền lương mỗi giờ: "))
+print("Tổng số tiền lương trong tháng là:", h * d * s)
