b1b264bd8691b618d97f5d01159339e33e7a60d6
Nduy <duy.nn.67khmt@ntu.edu.vn>
2025-12-29 14:56:29 +0700

    Create KiemTraCau1B.py

---
+++ KiemTraCau1B.py
@@ -0,0 +1,11 @@
+a = int(input("nhập số a: "))
+b = int(input("nhập số b: "))
+
+if a == 0 and b == 0:
+    print("phương trình vô số nghiệm")
+elif a == 0 and b != 0:
+    print("phương trình vô nghiệm")
+else:
+    x = -b / a
+    print("phương trình có nghiệm x =", x)
+
